ffc9fb93089d55f52226c5385ad25e9efc07b0ce
Astrid van Effrink <astridvaneffrink@hotmail.com>
2024-06-27 10:10:30 +0200

    RecipeListPage: Added all the functionalities

--- pages/RecipeListPage.jsx
+++ pages/RecipeListPage.jsx
@@ -1,13 +1,5 @@
-import { Center, Heading } from '@chakra-ui/react';
-import { data } from '../utils/data';
+import { RecipeList } from "../components/ui/RecipeList";
 
-export const RecipeListPage = () => {
-  // You can play around with the console log, but ultimately remove it once you are done
-  console.log(data.hits[0].recipe.label);
-
-  return (
-    <Center h="100vh" flexDir="column">
-      <Heading>Your Recipe App</Heading>
-    </Center>
-  );
+export const RecipeListPage = ({ items, clickFn }) => {
+  return <RecipeList items={items} clickFn={clickFn}></RecipeList>;
 };
