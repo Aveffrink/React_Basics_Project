b9a4cc09eebe0c2a648a281c10a23b9c216a0163
Astrid van Effrink <astridvaneffrink@hotmail.com>
2024-06-27 10:10:44 +0200

    App.jsx: added all the functionalities

--- App.jsx
+++ App.jsx
@@ -1,6 +1,23 @@
-import { RecipeListPage } from './pages/RecipeListPage';
+import { Box } from "@chakra-ui/react";
+import { useState } from "react";
+import { RecipePage } from "./pages/RecipePage";
+import { RecipeSearch } from "./components/ui/RecipeSearch";
+import { Header } from "./components/Header";
 
 export const App = () => {
-  // Your state code here
-  return <RecipeListPage />;
+  const [selectedItem, setSelectedItem] = useState();
+  return (
+    <>
+      <Header></Header>
+      <Box background="blue.100" paddingBottom="2rem">
+        {selectedItem ? (
+          <RecipePage item={selectedItem} clickFn={setSelectedItem} />
+        ) : (
+          <>
+            <RecipeSearch clickFn={setSelectedItem} />
+          </>
+        )}
+      </Box>
+    </>
+  );
 };
